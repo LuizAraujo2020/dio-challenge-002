3f4283004de40cd37c6e0ff7eaf12df025635f4d
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2023-08-26 10:44:37 -0300

    Initial Commit

---
+++ DIOSantanderDesadio02.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+let primeiroNome = "Steve"
+var sobrenome: String?
+
+print("\(primeiroNome) \(sobrenome ?? "Wozniak")")
+
+sobrenome = "Jobs"
+
+if let sobrenome {
+    print("\(primeiroNome) \(sobrenome)")
+}
